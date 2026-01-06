2bdc6673d03f30f6f275163739f8a893abed4a65
TheRDShmackleGuy <clubhousefarsi20@gmail.com>
2026-01-06 18:37:44 +0330

    Update Notifications.cs

--- EIOP/Core/Notifications.cs
+++ EIOP/Core/Notifications.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using EIOP.Tools;
+using System.Reflection;
+using EIOP.Tools; // Assumes your WAV parser is here
 using GorillaLocomotion;
 using UnityEngine;
 using UnityEngine.UI;
@@ -15,8 +17,9 @@ public class Notifications : MonoBehaviour
     private static   Notifications            Instance;
     private readonly Dictionary<Guid, string> notifications = new();
     private          GameObject               canvas;
-
-    private Text notificationText;
+    
+    private Text      notificationText;
+    private AudioClip notificationSound; // Variable to hold the sound
 
     private void Awake() => Instance = this;
 
@@ -40,6 +43,13 @@ public class Notifications : MonoBehaviour
 
         notificationText = canvas.GetComponentInChildren<Text>();
         canvas.SetLayer(XRSettings.isDeviceActive ? UnityLayer.FirstPersonOnly : UnityLayer.MirrorOnly);
+
+        // ----------------------------------------------------
+        // LOAD THE SOUND HERE
+        // ----------------------------------------------------
+        notificationSound = LoadWavFromResource("EIOP.Resources.NotificationSound.wav");
+        // ----------------------------------------------------
+
         ApplyNotificationText();
     }
 
@@ -50,6 +60,15 @@ public class Notifications : MonoBehaviour
         Instance.notifications[notificationId] = message;
         Instance.ApplyNotificationText();
         CoroutineManager.Instance.StartCoroutine(Instance.RemoveNotificationAfterTime(notificationId));
+
+        // ----------------------------------------------------
+        // PLAY THE SOUND HERE
+        // ----------------------------------------------------
+        if (Instance.notificationSound != null)
+        {
+            // We use the AudioSource from Plugin to prevent creating too many sources
+            Plugin.PlaySound(Instance.notificationSound);
+        }
     }
 
     private IEnumerator RemoveNotificationAfterTime(Guid notificationId)
@@ -76,5 +95,44 @@ public class Notifications : MonoBehaviour
 
         notificationText.supportRichText = true;
         notificationText.text            = text.Trim();
+    }
+
+    // Helper method to load the WAV file from the DLL
+    private AudioClip LoadWavFromResource(string resourcePath)
+    {
+        using Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourcePath);
+
+        if (stream == null)
+        {
+            Debug.LogError($"EIOP: Could not find notification sound at '{resourcePath}'. Check Build Action.");
+            return null;
+        }
+
+        byte[] buffer;
+        using (MemoryStream ms = new MemoryStream())
+        {
+            stream.CopyTo(ms);
+            buffer = ms.ToArray();
+        }
+
+        // Assumes you have the WAV parser in EIOP.Tools (same as used in Plugin.cs)
+        WAV     wav = new(buffer);
+        float[] samples;
+
+        if (wav.ChannelCount == 2)
+        {
+            samples = new float[wav.SampleCount];
+            for (int i = 0; i < wav.SampleCount; i++)
+                samples[i] = (wav.LeftChannel[i] + wav.RightChannel[i]) * 0.5f;
+        }
+        else
+        {
+            samples = wav.LeftChannel;
+        }
+
+        AudioClip audioClip = AudioClip.Create(resourcePath, wav.SampleCount, 1, wav.Frequency, false);
+        audioClip.SetData(samples, 0);
+
+        return audioClip;
     }
 }
